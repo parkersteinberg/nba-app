6f6c4b87418c99ea56c41813e134694fad7ed9ca
Parker Steinberg <parker.s52@gmail.com>
2023-02-13 22:26:49 -0700

    testing scaffolding

---
+++ __tests__/db.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it } from '@jest/globals'
+import { queryDatabase } from '@/database'
+import { fetchInjuryData } from '@/database/fetchInjuryData'
+
+describe('Testing db queries', () => {
+  describe('queryDatabase tests', () => {
+    it('Returns expected columns', () => {
+      // make db call
+      // check properties on results
+      // expect(results).toHaveShape
+    })
+  })
+})
